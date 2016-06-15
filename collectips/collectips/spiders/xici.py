ad7b2b07d6daba315808bcfcdb301b5f10c0eae7
yaoyang <yaoyang@mobvoi.com>
2016-06-15 20:02:04 +0800

    fix

--- collectips/collectips/spiders/xici.py
+++ collectips/collectips/spiders/xici.py
@@ -45,9 +45,7 @@ class XiciSpider(scrapy.Spider):
             pre_item['LAST_CHECK_TIME'] = ip.xpath('td[10]/text()')[0].extract()
 
             items.append(pre_item)
-
-        print(items)
-
+            
         return items
 
 
